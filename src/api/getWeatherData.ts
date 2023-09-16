53f59bb7eb2ae13c82245f0960c1379cbae458cc
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-16 18:25:58 -0300

    3 files modified

--- src/api/getWeatherData.ts
+++ src/api/getWeatherData.ts
@@ -1,6 +1,6 @@
 export default async function getWeatherData(query: string, API_KEY: string) {
   const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`
   );
 
   const res = await data.text();
--- src/components/weather/Main.svelte
+++ src/components/weather/Main.svelte
@@ -4,14 +4,35 @@
   //import IWeatherData from "../../api/types.ts";
 
   let data;
+  let loading;
 
   document.addEventListener("DOMContentLoaded", async function() {
-      data = await getWeatherData("Ponta%20Grossa", API_KEY);
+      loading = true;
+      data = await getWeatherData("Dubai", API_KEY);
+      loading = false;
   });
 </script>
 
 <div>
-  <h1>{data.name}</h1>
+{#if !loading}
+  <h1>{data?.name.toUpperCase()}</h1>
+
+  <div class="weather-card">
+    <h2>{data?.main?.temp.toFixed(1)}ºC</h2>
+    <span style="color: #0d82d5">MIN: {data?.main?.temp_min.toFixed(1)}ºC</span>
+    <span style="color: #f74528">MAX: {data?.main?.temp_max.toFixed(1)}ºC</span>
+  </div>
+{/if}
+{#if loading}
+  <h4>Carregando...</h4>
+{/if}
 </div>
 
-<style></style>
+<style>
+  .weather-card {
+    background-color: #002f68;
+    padding: 10px;
+    border-radius: 10px;
+    border: 4px solid #1572e540;
+  }
+</style>
