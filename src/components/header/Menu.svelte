04817e3b3ec33d594fe28363cb2420d26e52422c
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-19 18:41:26 -0300

    1 file modified

--- src/components/header/Menu.svelte
+++ src/components/header/Menu.svelte
@@ -1,12 +1,21 @@
-<div>
-  <button></button>
-  <button></button>
-  <button></button>
+<Script>
+function showInfo(){
+  window.alert("");
+}
+</script>
+
+<div class="flex">
+  <div>
+    <button on:click={showInfo}>Sobre</button>
+    <button disabled>App Mobile</button>
+    <button>Mais Apps</button>
+  </div>
 </div>
 
 <style>
-  div {
-    margin-left: auto;
-    margin-right: auto;
+  .flex {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
   }
 </style>
