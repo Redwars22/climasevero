8da4d843474ff29e3fb3ab9931ccfd25e0d1ad0f
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-19 17:40:18 -0300

    1 file modified

--- src/components/weather/Temperature.svelte
+++ src/components/weather/Temperature.svelte
@@ -5,9 +5,29 @@
   export let description;
 </script>
 
-<div>
-  <img src="https://openweathermap.org/img/wn/{icon}@2x.png"/>
+<div class="temperature-container">
+  <div class="vertical">
+    <img src="https://openweathermap.org/img/wn/{icon}@2x.png"/>
+    <h5>{description}</h5>
+  </div>
+  <div class="vertical">
     <h2>{temperature}ºC</h2>
     <span>Sensação: {feels_like}ºC</span>
-    <h5>{description}</h5>
+  </div>
 </div>
+
+<style>
+  .temperature-container {
+    display: flex;
+    justify-content: space-around;
+    border-bottom: 1px dashed lightblue;
+  }
+  
+  div {
+    display: flex;
+  }
+
+  .vertical {
+    flex-direction: column;
+  }
+</style>
