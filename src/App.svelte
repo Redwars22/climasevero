560410293fc3678b4bfe214e13af93b40e9641a3
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-19 18:32:51 -0300

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
   import viteLogo from '/vite.svg'
   import Header from './components/header/Header.svelte'
   import Main from './components/weather/Main.svelte'
+  import Menu from './components/header/Menu.svelte'
 </script>
 
 <main>
   <Header/>
+  <Menu/>
   <Main/>
 </main>
---
+++ src/components/header/Menu.svelte
@@ -0,0 +1,12 @@
+<div>
+  <button></button>
+  <button></button>
+  <button></button>
+</div>
+
+<style>
+  div {
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
