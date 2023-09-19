05c7c43ffc6fa347aac5d165c702ae3f98f6eed6
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-19 18:19:45 -0300

    1 file modified

--- src/utils/setBG.ts
+++ src/utils/setBG.ts
@@ -2,6 +2,7 @@ const BG_SET: string[] = [
   'https://www.wellahealth.com/blog/wp-content/uploads/2021/09/6-ways-to-stay-healthy-during-the-rainy-season.jpg',
   'https://cdn.pixabay.com/photo/2017/06/09/17/44/sky-2387734_1280.jpg',
   'https://images.pexels.com/photos/11019539/pexels-photo-11019539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg?cs=srgb&dl=pexels-francesco-ungaro-281260.jpg&fm=jpg',
 ];
 
 const setBG = (bg: number) => {
@@ -23,7 +24,7 @@ export const getWeatherBGFromString = (id: string) => {
       setBG(2);
       break;
     case 'Clear':
-      setBG(2);
+      setBG(3);
       break;
   }
 };
